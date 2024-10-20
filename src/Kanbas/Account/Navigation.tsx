73ba97ea9d74e238bdff93699076aff5beb79a6b
Ethan Tang <ethantang550@gmail.com>
2024-10-19 20:25:23 -0400

    a3 commit 2

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,11 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function AccountNavigation() {
+  const location = useLocation();
+
   return (
-    <div id="wd list-group wd-account-navigation">
-      <Link to={`/Kanbas/Account/Signin`} className="list-group-item active border border-0" > Signin  </Link> <br />
-      <Link to={`/Kanbas/Account/Signup`} className="list-group-item text-danger border border-0" > Signup  </Link> <br />
-      <Link to={`/Kanbas/Account/Profile`} className="list-group-item text-danger border border-0" > Profile </Link> <br />
+    <div className="list-group wd-account-navigation">
+      <Link
+        to="/Kanbas/Account/Signin"
+        className={`list-group-item border border-0 rounded-0 ${location.pathname === '/Kanbas/Account/Signin' ? 'active-link' : 'text-danger'}`}>
+        Signin
+      </Link>
+      <Link
+        to="/Kanbas/Account/Signup"
+        className={`list-group-item border border-0 rounded-0 ${location.pathname === '/Kanbas/Account/Signup' ? 'active-link' : 'text-danger'}`}>
+        Signup
+      </Link>
+      <Link
+        to="/Kanbas/Account/Profile"
+        className={`list-group-item border border-0 rounded-0 ${location.pathname === '/Kanbas/Account/Profile' ? 'active-link' : 'text-danger'}`}>
+        Profile
+      </Link>
     </div>
   );
 }
